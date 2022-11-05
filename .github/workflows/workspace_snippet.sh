d6c1d5c899e5ac0a3874e06636a86d5865744bb9
Ryan Leary <rleary@nvidia.com>
2022-11-04 23:12:25 -0400

    ci: minor cleanup

--- .github/workflows/workspace_snippet.sh
+++ .github/workflows/workspace_snippet.sh
@@ -4,12 +4,12 @@ set -o errexit -o nounset -o pipefail
 
 # Set by GH actions, see
 # https://docs.github.com/en/actions/learn-github-actions/environment-variables#default-environment-variables
-TAG=${GITHUB_REF_NAME}
+TAG=$1
 PREFIX="rules_cuda-${TAG:1}"
 SHA=$(git archive --format=tar --prefix=${PREFIX}/ ${TAG} | gzip | shasum -a 256 | awk '{print $1}')
 
 cat << EOF
-WORKSPACE snippet:
+## `WORKSPACE` code
 \`\`\`starlark
 load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
 http_archive(
